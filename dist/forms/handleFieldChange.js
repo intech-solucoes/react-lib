eae1f954b443e7616508e3f4a609fa1c0187d6d7
Rony <ronymmoura@gmail.com>
2019-08-26 16:51:43 -0300

    Correção na função handleFieldChange

--- dist/forms/handleFieldChange.js
+++ dist/forms/handleFieldChange.js
@@ -3,20 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function handleFieldChange(context, event, parent) {
     var _a, _b;
     var parent = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
-    var _c = event.target, name = _c.name, value = _c.value, maxLength = _c.maxLength;
-    if (maxLength && maxLength > 0)
-        value = value.slice(0, maxLength);
-    if (parent) {
-        var parentObj = context.state[parent];
-        parentObj[name] = value;
-        context.setState((_a = {},
-            _a[parent] = parentObj,
-            _a));
-    }
-    else {
-        context.setState((_b = {},
-            _b[name] = value,
-            _b));
+    var _c = event.target, name = _c.name, value = _c.value, maxLength = _c.maxLength, disabled = _c.disabled;
+    if (!disabled) {
+        if (maxLength && maxLength > 0)
+            value = value.slice(0, maxLength);
+        if (parent) {
+            var parentObj = context.state[parent];
+            parentObj[name] = value;
+            context.setState((_a = {},
+                _a[parent] = parentObj,
+                _a));
+        }
+        else {
+            context.setState((_b = {},
+                _b[name] = value,
+                _b));
+        }
     }
 }
 exports.default = handleFieldChange;
--- src/forms/handleFieldChange.ts
+++ src/forms/handleFieldChange.ts
@@ -1,21 +1,23 @@
 export default function handleFieldChange(context: any, event: any, parent?: any | null) {
     var parent = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
 
-    var { name, value, maxLength } = event.target;
+    var { name, value, maxLength, disabled } = event.target;
 
-    if(maxLength && maxLength > 0)
-        value = value.slice(0, maxLength);
+    if(!disabled) {
+        if(maxLength && maxLength > 0)
+            value = value.slice(0, maxLength);
 
-    if(parent) {
-        var parentObj = context.state[parent];
-        parentObj[name] = value;
+        if(parent) {
+            var parentObj = context.state[parent];
+            parentObj[name] = value;
 
-        context.setState({
-            [parent]: parentObj
-        });
-    } else {
-        context.setState({
-            [name]: value
-        });
+            context.setState({
+                [parent]: parentObj
+            });
+        } else {
+            context.setState({
+                [name]: value
+            });
+        }
     }
 }
